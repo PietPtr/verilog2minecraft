4ffcf8857a724997ce08975fe35c0c48cf716396
Daan Middelkoop <daantje676@gmail.com>
2021-12-10 21:48:17 +0100

    Initial test.py

--- minecraft/test.py
+++ minecraft/test.py
@@ -3,14 +3,14 @@ from amulet.api.block import Block
 
 # load the level
 level = amulet.load_level("data/flat")
-game_version = ("bedrock", (1, 18, 1))
-
+game_version = ("java", (1, 18, 1))
+print(level.dimensions)
 block = Block("minecraft", "stone")
 level.set_version_block(
     0,  # x location
     4,  # y location
     0,  # z location
-    "overworld",  # dimension
+    "minecraft:overworld",  # dimension
     game_version,
     block,
 )
