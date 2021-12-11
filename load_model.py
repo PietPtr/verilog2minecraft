da53829502480701b545bb742b0e936b6f8d855b
Daan Middelkoop <daantje676@gmail.com>
2021-12-11 01:29:44 +0100

    Update gate models

--- load_model.py
+++ load_model.py
@@ -3,9 +3,11 @@ from minecraft.world import World
 
 if __name__ == "__main__":
     if len(sys.argv) != 5:
-        print("Usage: python load_model.py <world path> <origin coords> <sizes> <name>")
+        print("Usage: python load_model.py <world path> <origin coords> <end coords> <name>")
         print("E.g. python load_model.py /home/daan/.minecraft/saves/design 0,2,0 3,2,2 flipflop")
     world = World()
-    model = world.read_model(sys.argv[1], tuple(map(int, sys.argv[2].split(','))), tuple(map(int, sys.argv[3].split(','))))
+    start = tuple(map(int, sys.argv[2].split(',')))
+    end = tuple(map(int, sys.argv[3].split(',')))
+    model = world.read_model(sys.argv[1], start, (end[0] - start[0], end[1] - start[1], end[2] - start[2]))
     with open(f'minecraft/data/components/{sys.argv[4]}', 'wb') as f:
         pickle.dump(model, f)
